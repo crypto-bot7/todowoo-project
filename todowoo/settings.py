a621490fa9c2de2b360b2feee355f4e6efccc469
piyush <piyushatghara@gmail.com>
2021-04-25 18:48:44 +0000

    Changed the settings file

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=4+dyq&=nvwo4!euy!xsx==ah=m)ik0+_01yb+8ov53vhfp1+t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cryptobot7.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_URL = '/login/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
