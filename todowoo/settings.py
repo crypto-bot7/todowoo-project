84ebae2384e7eb49808d3ef5e145b7b55d7dde08
piyush <piyushatghara@gmail.com>
2021-04-26 00:44:47 +0530

    fixed gitignore and settings

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -126,3 +126,8 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file, looks like you are on production")
